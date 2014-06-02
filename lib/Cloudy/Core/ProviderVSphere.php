919344442d41062ecea00bdff957563b1a599d04
Alexis Ducastel <alexis@ducastel.net>
2014-06-03 00:28:12 +0200

    Adding comments and providers configuration sample

--- lib/Cloudy/Core/ProviderVSphere.php
+++ lib/Cloudy/Core/ProviderVSphere.php
@@ -2,12 +2,28 @@
 namespace Cloudy\Core;
 
 class ProviderVSphere extends Provider{
+    /**
+    * Internal vSphere connector
+    * @var \Vmwarephp\Vhost
+    */
     protected $connector;
     
+    /**
+    * VmWare vSphere Provider
+    * @param string $host   Hostname or ip of vCenter/vHost
+    * @param int $port  Usually 443
+    * @param string $user   User account name
+    * @param mixed $pwd     User account password
+    * @return ProviderVSphere
+    */
     public function __construct($host,$port,$user,$pwd){
         $this->connector = new \Vmwarephp\Vhost($host.':'.$port, $user, $pwd);
     }
     
+    /**
+    * Get server list
+    * @return array
+    */
     public function serverList(){
         $serverList=array();
         $vmList = $this->connector->findAllManagedObjects('VirtualMachine', array('name'));
